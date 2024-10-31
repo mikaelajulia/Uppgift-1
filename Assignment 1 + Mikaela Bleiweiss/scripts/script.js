1116712955dccc811c1e41a981e64eb9334f4537
mikaelajulia <mikaela.bleiweiss@gritacademy.se>
2024-10-31 18:54:08 +0100

    Uppgift 1

---
+++ Assignment 1 + Mikaela Bleiweiss/scripts/script.js
@@ -0,0 +1,28 @@
+alert ("You can't devide us!")
+document.write()
+console.log(0)
+
+
+
+let foo = 0; // 5
+let bar = 0; // 10
+
+
+
+const divideButton = document.getElementById('divideButton');
+
+divideButton.addEventListener('click', function() {
+  foo = parseFloat(document.getElementById('fooInput').value); // 5
+  bar = parseFloat(document.getElementById('barInput').value); // 10
+
+  const result = foo / bar; // Dela talen
+
+  }
+);
+
+
+
+
+
+
+
